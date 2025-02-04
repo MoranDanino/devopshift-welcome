bdc015dd0450f25c23c15ba8032f68deae50d5e3
Your Name <you@example.com>
2025-02-04 13:45:28 +0000

    adding time provider for output ip "depends on"

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -1,9 +1,22 @@
+terraform {
+  required_providers {
+    time = {
+      source  = "hashicorp/time"
+      version = "0.12.1"  # Make sure to use the version that match latest version
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 variable "region" {
   default = "us-east-1"
+}
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "10s"  # Wait for 10 seconds
 }
   
 resource "aws_security_group" "sg" {
@@ -35,5 +48,6 @@ resource "aws_instance" "vm" {
 
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
+  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
   description = "Public IP address of the VM"
 }
